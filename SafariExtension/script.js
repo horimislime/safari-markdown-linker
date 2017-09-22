49565f651fac0e6fdb5435be8020adbad5ad002c
horimislime <horimislime@gmail.com>
2017-09-22 23:22:05 +0900

    Use ES6 syntax

--- SafariExtension/script.js
+++ SafariExtension/script.js
@@ -1,20 +1,21 @@
+'use strict';
 
-function notifyTargetLink(title, url) {
-    var absoluteURL = url.startsWith('/') ? location.protocol + '//' + location.host + location.pathname + url : url
-    safari.extension.dispatchMessage("linkInfo", {
-      "title": title,
-      "url": url
-    });
-}
+const notifyTargetLink = (title, url) => {
+  const absoluteURL = url.startsWith('/') ? `${location.protocol}//${location.host}${location.pathname}${url}` : url;
+  safari.extension.dispatchMessage('linkInfo', {
+    title: title,
+    url: absoluteURL
+  });
+};
 
-document.addEventListener("contextmenu", function(event) {
+document.addEventListener('contextmenu', (event) => {
 
-  var parent = event.srcElement.parentElement;
+  const parent = event.srcElement.parentElement;
   const clickedLink = Array.from(parent.getElementsByTagName('a')).filter((element) => {
-     return element.innerHTML == window.getSelection().toString();
-   })[0];
+    return element.innerHTML === window.getSelection().toString();
+  })[0];
 
-  if (parent.tagName == 'a') {
+  if (parent.tagName === 'a') {
     notifyTargetLink(parent.innerHTML, parent.href);
 
   } else if (clickedLink) {
