50a151def00520dfeeafe39e9c1c888de0f8eecc
horimislime <horimislime@gmail.com>
2017-09-22 23:14:38 +0900

    Group formatting logic

--- SafariExtension/SafariExtensionHandler.swift
+++ SafariExtension/SafariExtensionHandler.swift
@@ -33,26 +33,29 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     
     override func contextMenuItemSelected(withCommand command: String, in page: SFSafariPage, userInfo: [String : Any]? = nil) {
         
-        NSPasteboard.general().clearContents()
-        
-        if let title = lastLinkDetail?["title"] as? String, let urlString = lastLinkDetail?["url"] as? String {
-            NSPasteboard.general().setString("![\(title)](\(urlString))", forType: NSPasteboardTypeString)
+        if let title = lastLinkDetail?["title"] as? String, let urlString = lastLinkDetail?["url"] as? String, let url = URL(string: urlString) {
+            copyAsMarkdown(withTitle: title, url: url)
             return
         }
         
         page.getPropertiesWithCompletionHandler { properties in
-            
             guard let title = properties?.title, let url = properties?.url else { return }
+            self.copyAsMarkdown(withTitle: title, url: url)
+        }
+    }
+    
+    private func copyAsMarkdown(withTitle title: String, url: URL) {
+        
+        NSPasteboard.general().clearContents()
+        
+        let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, url.pathExtension as CFString, nil)?
+            .takeRetainedValue() ?? "" as CFString
+        
+        if UTTypeConformsTo(uti, kUTTypeImage as CFString) {
+            NSPasteboard.general().setString("![\(title)](\(url.absoluteString))", forType: NSPasteboardTypeString)
             
-            let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, url.pathExtension as CFString, nil)?
-                .takeRetainedValue() ?? "" as CFString
-            
-            if UTTypeConformsTo(uti, kUTTypeImage as CFString) {
-                NSPasteboard.general().setString("![\(title)](\(url.absoluteString))", forType: NSPasteboardTypeString)
-                
-            } else {
-                NSPasteboard.general().setString("[\(title)](\(url.absoluteString))", forType: NSPasteboardTypeString)
-            }
+        } else {
+            NSPasteboard.general().setString("[\(title)](\(url.absoluteString))", forType: NSPasteboardTypeString)
         }
     }
 }
