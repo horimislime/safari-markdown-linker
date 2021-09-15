d9dd833a4c82395151056c3f3db707fb4b55adcd
horimislime <horimislime@gmail.com>
2021-09-15 23:55:28 +0900

    Use navigation

--- URL Linker/SceneDelegate.swift
+++ URL Linker/SceneDelegate.swift
@@ -16,9 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        window.rootViewController = UINavigationController(rootViewController: ViewController(style: .insetGrouped))
         window.makeKeyAndVisible()
         self.window = window
     }
-
 }
