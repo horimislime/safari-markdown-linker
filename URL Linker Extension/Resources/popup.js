da381520ed00fbb8b675b92518dd3307dcf87e42
horimislime <horimislime@gmail.com>
2021-10-23 01:17:40 +0900

    Add example feedback

--- URL Linker Extension/Resources/popup.js
+++ URL Linker Extension/Resources/popup.js
@@ -1,1 +1,10 @@
 console.log("Hello World!", browser);
+
+const onClick = (element) => {
+  document.getElementById("test").innerHTML = "hellooooooo";
+  const currentText = element.innerHTML;
+  element.innerHTML = "Copied!";
+  setTimeout(() => {
+    element.innerHTML = currentText;
+  }, 1000);
+};
