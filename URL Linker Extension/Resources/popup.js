139a9f30ab3aab3ffd9907555b4b50133f25525d
horimislime <horimislime@gmail.com>
2021-10-24 00:19:53 +0900

    Communicate with html to copy page data

--- URL Linker Extension/Resources/popup.js
+++ URL Linker Extension/Resources/popup.js
@@ -1,10 +1,32 @@
-console.log("Hello World!", browser);
+browser.runtime.sendNativeMessage({ request: "getFormats" }).then((response) => {
+    console.log("success");
+    console.log("Received response: ", response);
+    const container = document.getElementById("container");
+    for (const format of response) {
+        const button = document.createElement("button");
+        button.id = format.command
+        button.className = "border-2 border-blue-400 text-blue-400 px-4 py-2 m-2 rounded text-xl";
+        button.innerText = format.name;
+        button.addEventListener("click", onClick);
+        container.appendChild(button);
+    }
+});
 
-const onClick = (element) => {
-  document.getElementById("test").innerHTML = "hellooooooo";
-  const currentText = element.innerHTML;
-  element.innerHTML = "Copied!";
-  setTimeout(() => {
-    element.innerHTML = currentText;
-  }, 1000);
+const onClick = (event) => {
+    const element = event.srcElement;
+    console.log("Clicked!", element);
+    const selectedText = window.getSelection().toString();
+    const title = selectedText.length === 0 ? document.title : selectedText;
+    const payload = {
+        title: title,
+        link: location.href,
+        command: element.id
+    }
+    browser.runtime.sendNativeMessage({request: "copy", payload: payload}).then((response) => {
+        const currentText = element.innerHTML;
+        element.innerHTML = "Copied!";
+        setTimeout(() => {
+            element.innerHTML = currentText;
+        }, 1000);
+    });
 };
--- URL Linker Extension/SafariWebExtensionHandler.swift
+++ URL Linker Extension/SafariWebExtensionHandler.swift
@@ -13,13 +13,35 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
 
     func beginRequest(with context: NSExtensionContext) {
         let item = context.inputItems[0] as! NSExtensionItem
-        let message = item.userInfo?[SFExtensionMessageKey]
-        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
-
+        guard let message = item.userInfo?[SFExtensionMessageKey] as? [String: AnyObject] else {
+            os_log(.default, "Invalid native request payload")
+            return
+        }
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as CVarArg)
+        
+        let setting = Setting.load() ?? Setting.default
         let response = NSExtensionItem()
-        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+        if message["request"] as! String == "getFormats" {
+            let formats = setting.urlFormats.map { ["name": $0.name, "command": $0.commandName] }
+            response.userInfo = [SFExtensionMessageKey: formats]
+        } else if message["request"] as! String == "copy" {
+            let payload = message["payload"] as! [String: String]
+            let title = payload["title"]!
+            let link = payload["link"]!
+            let command = payload["command"]!
+            guard let format = setting.urlFormats.first(where: { $0.commandName == command }) else { return }
+            let formattedString = format.pattern
+                .replacingOccurrences(of: "%URL", with: link)
+                .replacingOccurrences(of: "%TITLE", with: title)
+            UIPasteboard.general.string = formattedString
+        } else {
+            
+        }
+        
+        os_log(.default, "sendNativeMessage: Load setting")
+        
+        
 
         context.completeRequest(returningItems: [response], completionHandler: nil)
     }
-
 }
