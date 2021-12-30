fa50e37e781cd5324e71f8853e80f920985e9e08
horimislime <horimislime@gmail.com>
2021-12-30 23:52:27 +0900

    Highlight button on tap

--- iOS (Extension)/Resources/popup.js
+++ iOS (Extension)/Resources/popup.js
@@ -3,7 +3,7 @@ browser.runtime.sendNativeMessage({ request: "getFormats" }).then((response) => 
     for (const format of response) {
         const button = document.createElement("button");
         button.id = format.command
-        button.className = "bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 m-2 rounded";
+        button.className = "bg-blue-500 active:bg-blue-700 text-white font-bold px-4 py-2 m-2 rounded";
         button.innerText = format.name;
         button.addEventListener("click", onClick);
         container.appendChild(button);
